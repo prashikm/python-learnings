8823abe76e31f8e414b2eac989448f94e3ab9ded
Prashik Meshram <prashikm861@gmail.com>
2022-02-05 17:49:48 +0530

    fix identity operator values

--- 02_Operators & Operations/2_operators.py
+++ 02_Operators & Operations/2_operators.py
@@ -12,7 +12,7 @@ print('5 * 2: ', Multiplication)   # 10
 print('8 // 2: ', Division)        # 4.0
 print('7 % 2: ', Modulus)          # 1
 print('2 ** 2: ', Exponential)     # 4
-
+print('\n')                        # adds new line below
 
 # ----------------------- Assignment Operators ------------------------------
 x = 5
@@ -22,50 +22,50 @@ x += 4
 print('x += 4: ', x)
 
 x -= 2
-print('x -= 2', x)
+print('x -= 2: ', x)
 
 x *= 3
-print('x *= 3', x)
+print('x *= 3: ', x)
 
 x //= 6
-print('x //= 6', x)
+print('x //= 6: ', x)
 
 x %= 3
-print('x %= 3', x)
+print('x %= 3: ', x)
 
 x **= 2
-print('x **= 2', x)
+print('x **= 2: ', x)
+print('\n')
 
 
 # ----------------------- Comparison Operators ------------------------------
 a = 14
 b = 100
 
-print('a == b', a == b)
-print('8 == 8', 8 == 8)
+print('a == b: ', a == b)
+print('8 == 8: ', 8 == 8)
 
-print('a != b', a != b)
-print('45 != 45', 45 != 45)
+print('a != b: ', a != b)
+print('45 != 45: ', 45 != 45)
 
-print('a > b', a > b)
-print('a < b', a < b)
-print('a >= b', a >= b)
-print('a <= b', a <= b)
+print('a > b: ', a > b)
+print('a < b: ', a < b)
+print('a >= b: ', a >= b)
+print('a <= b: ', a <= b)
+print('\n')
 
 
 # ----------------------- Logical Operators ------------------------------
 bool1 = True
 bool2 = False
 
-print("The value of bool1 AND bool2 is", bool1 and bool2)
-print("The value of bool1 OR bool2 is", bool1 or bool2)
-print("The value of NOT bool1 is", not bool1)
-print("The value of NOT bool2 is", not bool2)
+print("The value of bool1 AND bool2 is: ", bool1 and bool2)
+print("The value of bool1 OR bool2 is: ", bool1 or bool2)
+print("The value of NOT bool1 is: ", not bool1)
+print("The value of NOT bool2 is: ", not bool2)
+print('\n')
 
 
 # ----------------------- Identity Operators ------------------------------
-print('64 is 23', 64 is 23)
-print('22 is 22', 22 is 22)
-
-print('64 is 23', 64 is 23)
-print('22 is not 22', 22 is not 22)
+print('[2, 4, 6, 8] is [2, 4, 6, 8]: ', [2, 4, 6, 8] is [2, 4, 6, 8])
+print('\n')
